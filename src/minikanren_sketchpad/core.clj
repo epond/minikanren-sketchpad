16cb3a74a122ba9ce797a54fb6504010607a2d9b
epond <epond@users.noreply.github.com>
2016-02-28 10:31:37 +0000

    Improve structure

--- src/minikanren_sketchpad/core.clj
+++ src/minikanren_sketchpad/core.clj
@@ -3,65 +3,60 @@
   (:use clojure.core.logic
         clojure.core.logic.pldb))
 
-(db-rel mano x)
-(db-rel womano x)
+(defn -main [& args]
 
-(def facts
-  (db
-    [mano :alan-turing]
-    [womano :grace-hopper]
-    [mano :leslie-lamport]
-    [mano :alonzo-church]
-    [womano :ada-lovelace]
-    [womano :barbara-liskov]
-    [mano :john-mccarthy]
-    ))
+  ; ===============================
+  ; Day 1: Unified Theories of Code
+  ; ===============================
 
-(db-rel vitalo p s)
-(db-rel turingo p y)
+  (db-rel mano x)
+  (db-rel womano x)
 
-(def facts
-  (-> facts
-      (db-fact vitalo :alan-turing :dead)
-      (db-fact vitalo :grace-hopper :dead)
-      (db-fact vitalo :leslie-lamport :alive)
-      (db-fact vitalo :alonzo-church :dead)
-      (db-fact vitalo :ada-lovelace :dead)
-      (db-fact vitalo :barbara-liskov :alive)
-      (db-fact vitalo :john-mccarthy :dead)
-      (db-fact turingo :leslie-lamport :2013)
-      (db-fact turingo :barbara-liskov :2008)
-      (db-fact turingo :john-mccarthy :1971)
+  (def facts
+    (db
+      [mano :alan-turing]
+      [womano :grace-hopper]
+      [mano :leslie-lamport]
+      [mano :alonzo-church]
+      [womano :ada-lovelace]
+      [womano :barbara-liskov]
+      [mano :john-mccarthy]
       ))
 
-(defn insideo [e l]
-  (conde
-    [(fresh [h t]
-            (conso h t l)
-            (== h e))]
-    [(fresh [h t]
-            (conso h t l)
-            (insideo e t))]
-    ))
+  (db-rel vitalo p s)
+  (db-rel turingo p y)
 
-(defn -main [& args]
+  (def facts
+    (-> facts
+        (db-fact vitalo :alan-turing :dead)
+        (db-fact vitalo :grace-hopper :dead)
+        (db-fact vitalo :leslie-lamport :alive)
+        (db-fact vitalo :alonzo-church :dead)
+        (db-fact vitalo :ada-lovelace :dead)
+        (db-fact vitalo :barbara-liskov :alive)
+        (db-fact vitalo :john-mccarthy :dead)
+        (db-fact turingo :leslie-lamport :2013)
+        (db-fact turingo :barbara-liskov :2008)
+        (db-fact turingo :john-mccarthy :1971)
+        ))
+
   ; membero is a relation that says its first argument is a member of the list in the second argument.
-  ; What are the members of the list [1 2 3]?
-  (println (run* [q] (membero q [1 2 3])))
-  ; What lists have 1 as a member? (limit to 2 results)
-  (println (run 2 [q] (membero 1 q)))
+  (println "What are the members of the list [1 2 3]?"
+           (run* [q] (membero q [1 2 3])))
+  (println "What lists have 1 as a member (limit to 2 results)?"
+           (run 2 [q] (membero 1 q)))
 
-  ; Who are the women?
-  (println (with-db facts
+  (println "Who are the women?"
+           (with-db facts
                     (run* [q]
                           (womano q))))
-  ; Which women are alive?
-  (println (with-db facts
+  (println "Which women are alive?"
+           (with-db facts
                     (run* [q]
                           (womano q)
                           (vitalo q :alive))))
-  ; Which deceased people won the Turing Award?
-  (println (with-db facts
+  (println "Which deceased people won the Turing Award?"
+           (with-db facts
                     (run* [q]
                           (fresh [p y]
                                  (vitalo p :dead)
@@ -78,6 +73,17 @@
   (println (run* [q] (conso :a [:b :c] q)))
 
   ; insideo is equivalent to the built-in membero
+  (defn insideo [e l]
+    (conde
+      [(fresh [h t]
+              (conso h t l)
+              (== h e))]
+      [(fresh [h t]
+              (conso h t l)
+              (insideo e t))]
+      ))
   (println (run* [q] (insideo q [:a :b :c])))
   (println (run* [q] (insideo :d [:a :b :c q])))
+
+
   )
